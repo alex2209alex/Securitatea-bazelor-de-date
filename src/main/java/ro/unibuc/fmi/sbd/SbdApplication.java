58fe2835c05bfb02f46433a235a7f5e6f3e765a6
Alexandru Pavel <alex.pavel2002@gmail.com>
2026-01-19 22:53:27 +0200

    Commit initial

---
+++ src/main/java/ro/unibuc/fmi/sbd/SbdApplication.java
@@ -0,0 +1,11 @@
+package ro.unibuc.fmi.sbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbdApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SbdApplication.class, args);
+	}
+}
